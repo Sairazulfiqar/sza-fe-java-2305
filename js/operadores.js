6472ada840df663f7fdbd7af9539d92c210018f4
saaiira a <sairazulfiqaranjum@gmail.com>
2023-05-23 16:35:03 +0200

    js

---
+++ js/operadores.js
@@ -0,0 +1,30 @@
+let valorA = 10;
+let valorB =4;
+
+let resultado = 0; 
+
+//operadores aritmeticos
+
+resultado = valorA + valorB;
+resultado = 'Bienvenido A' + 'BCN'
+resultado = valorA - valorB;
+resultado = valorA * valorB;
+resultado = 10 / 2;
+resultado = 10 % 2; //10 % 2 ==0
+resultado = 2**3;
+
+resultado = Math.pow (2,3);
+
+//operadores lógicos ( > < >= <= == ! = === !== ) y para concatenar && ||
+
+resultado = valorA <= 0;
+resultado = valorA == 0;
+resultado = valorA !=0;
+resultado = valorA ===0; 
+resultado = valorA > 2 || valorA == 10;
+resultado = valorA > 2 && valorA == 10;
+
+if (valorA > 2 || valorA == 10) {
+    console.log('valorA es mayor que 2');
+}
+
---
+++ js/variables.js
@@ -0,0 +1,11 @@
+//tipos de variables en js
+var nombre = 'Saira'
+let edad = 34
+const esalumna = true;
+const IVA = 0.21;
+
+console.log(nombre);
+
+nombre= 1234;
+
+console.log(nombre);
